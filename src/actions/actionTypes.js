44ab0399a9a10f053b331f0606b54f95fbc3bfd4
som2y <samyin1990@gmail.com>
2019-10-02 21:27:15 +1300

    Updates

--- src/actions/actionTypes.js
+++
@@ -1,4 +0,0 @@
-export const AddProduct = 'ADD_PRODUCT'
-export const RemoveProduct = 'REMOVE_PRODUCT'
-export const GetProducts = 'GET_PRODUCTS'
-export const GetProductsSuccess = 'GET_PRODUCTS_SUCCESS'
--- src/sagas/index.js
+++
@@ -1,8 +0,0 @@
-import { fork, all } from 'redux-saga/effects'
-import productSaga from './productSaga'
-
-export default function* rootSaga() {
-    yield all([
-        fork(productSaga)
-    ])
-}
